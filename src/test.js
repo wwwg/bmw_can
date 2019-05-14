006a3802a4675a050f170f5775ead3c1a1dd51ed
wwwg <joshpauten@gmail.com>
2019-05-13 20:49:25 -0500

    Construct e90Serial after interface is located

--- src/test.js
+++ src/test.js
@@ -1,5 +1,11 @@
 const e90Serial = require('./e90Serial');
 
 e90Serial.locateCANInterface().then(canInterface => {
-	console.log(canInterface);
+	console.log('Detected D-CAN interface:');
+	console.log(canInterface.pnpId);
+	// create e90 serial connection with located interface
+	let e90 = new e90Serial(canInterface.comName);
+	e90.on('open', () => {
+		console.log('e90 serial port opened!');
+	})
 })
