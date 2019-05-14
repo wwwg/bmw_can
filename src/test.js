4cd5bc29a3385c917a4152e7afe18d737a088244
wwwg <joshpauten@gmail.com>
2019-05-13 20:47:31 -0500

    Update interface detection

--- src/test.js
+++ src/test.js
@@ -1,9 +1,5 @@
 const e90Serial = require('./e90Serial');
 
-e90Serial.locateCANInterface();
-/*
-let e90 = new e90Serial(canBus.com)
-e90.on('open', () => {
-	console.log('e90 opened!');
-});
-*/
+e90Serial.locateCANInterface().then(canInterface => {
+	console.log(canInterface);
+})
